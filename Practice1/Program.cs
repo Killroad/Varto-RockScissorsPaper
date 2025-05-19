3bf7b126b098b670ddd9d32d4d9934e05f6f3a51
Valerii N <killroad90@gmail.com>
2025-05-19 19:37:30 +0300

    Initial project setup: added Program.cs and basic structure

---
+++ Practice1/Program.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace Practice1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            // TODO: Ask user's name and age
+            RunGame();
+        }
+        
+        static void RunGame()
+        {
+            // TODO: Play the game loop
+            // TODO: Show statistics
+        }
+        
+        // TODO: Add more methods
+        
+    }
+}
